9831952f8b052b0d5434c79b563892bcb850ce87
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2016-04-19 10:28:43 +0200

    home css

--- src/main/resources/static/js/movie-images.js
+++ src/main/resources/static/js/movie-images.js
@@ -1,11 +1,11 @@
 $(function() {
   setTimeout(function() {
-      $films = $('.owl-item div');
+      $films = $('.owl-item > div');
       var time = 1000;
       $films.each(function() {
           var $film = $(this);
           setTimeout(function() {
-            $.getJSON('http://www.faroo.com/api?i=true&q=' + escape(/*$film.text()*/'avengers') + '&start=1&length=10&l=en&src=web&i=false&f=json&key=qp1XSHLT@1jP8WX-g@RlXaMhQvA_', function(data) {
+            $.getJSON('http://www.faroo.com/api?i=true&q=' + escape($film.text()) + '&start=1&length=10&l=en&src=web&i=false&f=json&key=qp1XSHLT@1jP8WX-g@RlXaMhQvA_', function(data) {
               var imageUrl;
               console.log(data);
               data.results.forEach(function(result) {
@@ -16,6 +16,11 @@ $(function() {
               $film.css('backgroundImage', 'url(' + imageUrl + ')')
             });
           }, time);
+          $film.mousedown(function() {
+            $films.removeClass('focused');
+            $film.addClass('focused');
+            $('.details h1').text($film.text());
+          });
           time += 1200;
       });
   }, 100);
